05ec51870d9c7a3a5771f04041b7e9bb5f348e9b
triks <triks0001000@gmail.com>
2024-03-02 21:41:13 +0300

    dell k6

--- text.py
+++ text.py
@@ -1,14 +1,14 @@
 
 
 
-len_text = 120
-text = 'grafana timescaledb settings'
+# len_text = 120
+# text = 'grafana timescaledb settings'
 
-text = f' | {text.upper()} | '
+# text = f' | {text.upper()} | '
 
-print('# ' + '=' * len_text)
-print('# ' + f'| {text:-^{len_text - 4}} |')
-print('# ' + '=' * len_text)
+# print('# ' + '=' * len_text)
+# print('# ' + f'| {text:-^{len_text - 4}} |')
+# print('# ' + '=' * len_text)
 
 
 
